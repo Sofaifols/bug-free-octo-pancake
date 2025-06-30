b36c9a799e3ce69bc636c30cc33ec6edeeb57dbd
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-06-30 16:21:00 +0000

    operadors

---
+++ .vscode/operadoresatribuicao.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(){
+/* 
+ATRIBUIÇÃO SIMPLES (=)
+ATRIBUIÇÃO COM SOMA (+=)
+ATRIBUIÇÃO COM SUBTRAÇÃO (-=)
+ATRIBUIÇÃO COM MULTIPLICAÇÃO(*=)
+ATRIBUIÇÃO COM DIVISÃO(/=)
+*/
+
+
+int n1 = 10, n2, resultado;
+
+resultado = 10;
+printf("Resultado: %d \n", resultado);
+
+// resultado = resultado + 20;
+resultado +=20;
+printf("Resultado: %d \n", resultado);
+
+//resultado = resultado - numero1;
+resultado -= n1;
+printf("Resultado: %d \n", resultado);
+
+//resultado = resultado * 5;
+resultado *= 5;
+printf("Resultado: %d \n", resultado);
+
+//resultado = resultado / 2;
+resultado /=2;
+printf("Resultado: %d \n", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
