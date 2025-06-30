1843b076b5748fd8393c59b247fdec236f988db8
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-06-30 16:30:59 +0000

    config

---
+++ .vscode/operadoresIncrementoDecremento.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+
+/*
+INCREMENTO (++)
+Pré-incremento ++a
+Pós-incremento a++
+
+DECREMENTO (--)
+Pré-decremento --a
+Pós-decremento a--
+*/
+
+
+int 
+
+
+
+
+
+
+
+
+}
