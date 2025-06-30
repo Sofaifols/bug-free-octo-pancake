8ee46879b3505e26ff1783574453bfab35c7c854
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-06-30 18:39:10 +0000

    incremento e decremento

--- .vscode/operadoresIncrementoDecremento.c
+++ .vscode/operadoresIncrementoDecremento.c
@@ -7,18 +7,51 @@ int main(){
 INCREMENTO (++)
 Pré-incremento ++a
 Pós-incremento a++
+ACRESCENTA SOMENTE 1 UNIDADE
+
 
 DECREMENTO (--)
 Pré-decremento --a
 Pós-decremento a--
+DIMINUI SOMENTE 1 UNIDADE
 */
 
 
-int sd
+int numero1 =1, resultado;
+
+ printf("Antes incremento: %d \n", numero1);
+ numero1++;// numero1 = numero1 + 1;com pós incremento
+ // com atribuição: numero1 += 1;
+ //pós incremento
+ //resultado = numero1;
+ //numero++;
+ printf("Após incremento: %d \n", numero1);
+
+ resultado = ++numero1;
+printf("pré-incremento - número1:  %d - resultado %d \n", numero1, resultado);
 
 
 
 
+
+printf("Antes do decremento: %d - resultado: %d", numero1, resultado); 
+ numero1--;//numero1 = numero1 - 1;com decremento
+ //com atribuição numero1 -=1;
+
+ resultado = --numero1;
+ printf("Pré-decremento - numero1:  %d  - resultado %d \n", numero1, resultado);
+ //printf("Após decremento: %d \n", numero1); 
+
+
+
+
+
+
+
+
+
+// usar x++ quando quiser usar valor antigo e só depois atualizar
+// usar ++x quando precisar do valor já atualizado(incrementdao imediatamente)
 
 
 
