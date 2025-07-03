e8945cf04ddcbdb448b5907538d73f0f9be24091
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-07-03 02:55:18 +0000

    casting com comparacao2

---
+++ .vscode/castingeoperadoresrelacionais.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+
+    printf("número 1 > número2: %d \n", numero1 > numero2);
+    printf("número 1 == número 2: %d \n", numero1 == numero2);
+
+    //como são variaveis de tipo diferente, eu posso vir a usar o 
+    //casting como solução, para caso precise de algo com precisão
+    // ele vai depois das aspas e vírgula, e antes de chamar a varíavel
+    // entre parenteses
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
